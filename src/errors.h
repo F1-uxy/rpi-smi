4456d94096cbef66bb32021c9e6cf5dc5552522e
Charlie Flux <mail@charlieflux.com>
2026-04-28 16:15:06 +0100

    Added proper error codes

--- src/errors.h
+++ src/errors.h
@@ -22,4 +22,33 @@
     #define LOG(msg)
 #endif
 
+/* --- SMI Error Codes --- */
+
+#define SMI_OK 0
+
+/* General Errors */
+#define SMI_ERR_INVALID_ARG     -01  /* Invalid argument passed */
+#define SMI_ERR_NULL_PTR        -02  /* Null pointer passed */
+#define SMI_ERR_NULL_CXT        -03  /* Null context passed */
+
+/* Memory Errors */
+#define SMI_ERR_ALLOC_FAIL      -11  /* Heap allocation failed */
+#define SMI_ERR_BUF_TO_SMALL    -12  /* Buffer too small for transfer */
+#define SMI_ERR_MMAP_FAIL       -13  /* Memory mapping failed */
+#define SMI_ERR_FILE_IO_FAIL    -14
+
+/* Transfer Errors */
+#define SMI_ERR_TIMEOUT         -21  /* Transfer timeout reached */
+#define SMI_ERR_DMA_FAIL        -22  /* DMA transfer failed */
+
+/* Configuration Errors */
+#define SMI_ERR_INVALID_WIDTH   -31  /* Invalid bus width */
+#define SMI_ERR_INVALID_DEVICE  -32  /* Invalid device number */
+#define SMI_ERR_INVALID_ADDR    -33  /* Invalid SMI address */
+#define SMI_ERR_INVALID_FORMAT  -34  /* Invalid pixel format */
+
+#define SMI_ERR_TIMEOUT     -2
+#define SMI_ERR_DMA_FAIL    -3
+
+
 #endif
